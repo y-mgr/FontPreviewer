67533cbff89753e4ab73c3c35cccc58e9cb28675
MAGARA Yasuhiro <y-magara@mmi-sc.co.jp>
2021-04-15 11:32:39 +0900

    プロジェクト ファイルを追加します。

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace FontPreviewer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
